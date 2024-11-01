5cfd6a11ce706a4cad5e04c1a0243fc826a53f86
hlenarocha <helenavitoria1200@gmail.com>
2024-11-01 11:23:22 -0300

    [bugfix] fix image sources on AddItem

--- src/components/AddArea/index.tsx
+++ src/components/AddArea/index.tsx
@@ -1,18 +1,27 @@
-import * as C from './styles';
-import { AddItem } from '../AddItem'
-
+import * as C from "./styles";
+import { AddItem } from "../AddItem";
+import IncomeImage from "../../assets/income.png";
+import ExpenseImage from "../../assets/expense.png";
 interface Props {
   setIsOpenIncome: (isOpen: boolean) => void;
   setIsOpenExpense: (isOpen: boolean) => void;
 }
 
-export const AddArea = ({setIsOpenIncome, setIsOpenExpense}: Props) => {
- 
-  return( 
+export const AddArea = ({ setIsOpenIncome, setIsOpenExpense }: Props) => {
+  return (
     <C.Container>
-      <AddItem setIsOpen={setIsOpenIncome} title='Adicionar renda' source='../src/assets/income.png' subtitle='Criar uma renda manualmente'></AddItem>
-      <AddItem setIsOpen={setIsOpenExpense} title='Adicionar despesa' subtitle='Criar uma despesa manualmente' source='../src/assets/expense.png' />
+      <AddItem
+        setIsOpen={setIsOpenIncome}
+        title="Adicionar renda"
+        source={IncomeImage}
+        subtitle="Criar uma renda manualmente"
+      ></AddItem>
+      <AddItem
+        setIsOpen={setIsOpenExpense}
+        title="Adicionar despesa"
+        subtitle="Criar uma despesa manualmente"
+        source={ExpenseImage}
+      />
     </C.Container>
-
   );
-}
+};
